293581993b760b52632bcde84d365bd7f46ec389
Manfrinne <m4nfrinne@gmail.com>
2020-12-16 23:15:01 -0300

    Bug with the add element button on the Edit page resolved;

--- public/scriptAddItem.js
+++ public/scriptAddItem.js
@@ -1,37 +1,37 @@
+
+// Função para adicionar elemento na lista de ingredientes
 function addIngredient() {
   const ingredients = document.querySelector("#ingredients");
   const fieldContainer = document.querySelectorAll(".ingredient");
 
   // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
 
   // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
+  if (newField.children[0].value == "") return false
 
   // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
+  newField.children[0].value = ""
+  ingredients.appendChild(newField)
 }
 
+document.querySelector(".add-ingredient").addEventListener("click", addIngredient)
+
+
+// Função para adicionar elemento na lista de passos
 function addSteps() {
-  const ingredients = document.querySelector("#preparations-steps");
-  const fieldContainer = document.querySelectorAll(".preparations-step");
+  const ingredients = document.querySelector("#preparations-steps")
+  const fieldContainer = document.querySelectorAll(".preparations-step")
 
   // Realiza um clone do último ingrediente adicionado
-  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
 
   // Não adiciona um novo input se o último tem um valor vazio
-  if (newField.children[0].value == "") return false;
+  if (newField.children[0].value == "") return false
 
   // Deixa o valor do input vazio
-  newField.children[0].value = "";
-  ingredients.appendChild(newField);
+  newField.children[0].value = ""
+  ingredients.appendChild(newField)
 }
 
-document
-  .querySelector(".add-ingredient")
-  .addEventListener("click", addIngredient);
-
-document
-  .querySelector(".add-steps")
-  .addEventListener("click", addSteps);
+document.querySelector(".add-steps").addEventListener("click", addSteps)
